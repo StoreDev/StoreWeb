9506aab4d8e48057ee0b6b7048e010cbc2e1adc5
tuxuser <462620+tuxuser@users.noreply.github.com>
2020-09-01 04:03:19 +0200

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -20,7 +20,12 @@ namespace StoreWeb
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                       .UseStartup<Startup>()
+                       .UseKestrel(options =>
+                        {
+                            options.ListenAnyIP(Int32.Parse(System.Environment.GetEnvironmentVariable("PORT")));
+                        });
                 });
     }
 }
