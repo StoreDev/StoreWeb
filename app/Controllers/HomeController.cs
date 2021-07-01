523ebb2f504d1953289892df5c1a8f3aaff83d86
TitleOS <titleos@titleos.dev>
2021-07-01 14:50:22 -0500

    Added FAQ page, other minor tweaks.

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace StoreWeb.Controllers
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
